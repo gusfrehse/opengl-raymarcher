cb5a36fe0ef69ab0068bca6f3ea82080f57167a5
Gustavo S Frehse <gusfrehse@gmail.com>
2020-05-19 20:33:14 -0300

    Added camera class

---
+++ src/camera.cpp
@@ -0,0 +1,23 @@
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include "camera.hpp"
+
+Camera::Camera(glm::vec3 position, glm::vec3 fronti, glm::vec3 ups) :
+    pos(position), front(fronti), up(ups), right(glm::cross(front, up))
+{}
+
+void Camera::rotate(float angle, glm::vec3 axis)
+{
+    float angle2 = angle / 2;
+    glm::quat q = glm::quat(glm::cos(angle2), glm::sin(angle2) * axis);
+    glm::quat conjugate = glm::quat(glm::cos(angle2), -glm::sin(angle2) * axis);
+    front = q * front * conjugate;
+    up = q * up * conjugate;
+    right = q * up * conjugate;
+}
+
+void Camera::translate(glm::vec3 where)
+{
+    pos = where;
+}
---
+++ src/camera.hpp
@@ -0,0 +1,23 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+
+
+
+class Camera
+{
+public:
+    glm::vec3 pos;
+    glm::vec3 front;
+    glm::vec3 up;
+    glm::vec3 right;
+
+    Camera(glm::vec3 position, glm::vec3 fronti, glm::vec3 ups);
+    ~Camera();
+
+    void rotate(float angle, glm::vec3 axis);
+    void translate(glm::vec3 where);
+};
+#endif
